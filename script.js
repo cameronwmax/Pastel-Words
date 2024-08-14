504a535408c4e5b2450df4d41b59cf2bfc9b0c8f
cameronwmax <cameronwmax@icloud.com>
2024-08-14 10:45:38 -0700

    variable and key functionality

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+"use strict";
+import { WORDS } from "./words.js";
+
+let randInt = Math.floor(Math.random() * WORDS.length);
+let word = WORDS[randInt];
+word = "grape";
+
+let curRow = 0;
+let curTile = 0;
+let curWord = "";
+
+const rows = document.querySelectorAll(".row");
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((cur) => {
+  if (cur.dataset.key === "enter" || cur.dataset.key === "back") {
+    return;
+  }
+
+  cur.addEventListener("click", function () {
+    if (curTile === 5) return;
+
+    rows[curRow].querySelector(`.box-${curTile}`).innerHTML = cur.dataset.key;
+    curTile++;
+    curWord += cur.dataset.key;
+    console.log(curWord);
+  });
+});
